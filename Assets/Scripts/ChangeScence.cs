0bd510a022910ef6938a7de19b9a8e5e9df17015
TeilyMa <630924780@qq.com>
2019-05-26 11:21:22 +0800

    UI优化

--- Assets/Scripts/ChangeScence.cs
+++ Assets/Scripts/ChangeScence.cs
@@ -4,29 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScence : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Scene s = SceneManager.GetActiveScene();
-        GameObject _btn = new GameObject();
-        switch (s.buildIndex)
-        {
-            case 0:
-                _btn = GameObject.Find("Canvas/SelectPanel/Btn_Length");
-                break;
-            case 1:
-                _btn = GameObject.Find("Canvas/SelectPanel/Btn_Angle");
-                break;
-            case 2:
-                _btn = GameObject.Find("Canvas/SelectPanel/Btn_Distance");
-                break;
-            case 3:
-                _btn = GameObject.Find("Canvas/SelectPanel/Btn_Area");
-                break;
-        }
-        if (_btn != null)
-            _btn.GetComponent<Button>().interactable = false;
-    }
-
     public void ChangeToLengthMeasure() {
         SceneManager.LoadScene(0);
     }
